7b2e44753a6e3c3fe826d1e7997bba477ec0dc92
carmenluo <Sdluojm@gmail.com>
2019-10-20 20:51:01 -0400

    add content scrolling

---
+++ public/scripts/index.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  window.addEventListener("scroll", function() {
+
+    const slideInAt = window.scrollY + window.innerHeight;
+
+    const section2 = document.querySelector("#section2");
+    const section1DistanceToTop = window.pageYOffset + section2.getBoundingClientRect().top;
+    if (slideInAt > section1DistanceToTop + 60) {
+      section2.classList.add("showing");
+    } else {
+      section2.classList.remove("showing");
+    }
+
+    const historySection = document.querySelector("#history");
+    const historyDistanceToTop = window.pageYOffset + historySection.getBoundingClientRect().top;
+    if (slideInAt > historyDistanceToTop + 60) {
+      historySection.classList.add("showing");
+    } else {
+      historySection.classList.remove("showing");
+    }
+
+  });
+})
+
