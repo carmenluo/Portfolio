b0a2a5c8b3e20ab3f668eb44baa9eb48ba98bb1a
carmenluo <Sdluojm@gmail.com>
2019-10-20 20:59:59 -0400

    add showing effect on developer content

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,21 +2,21 @@ $(document).ready(function() {
   window.addEventListener("scroll", function() {
 
     const slideInAt = window.scrollY + window.innerHeight;
+    console.log(slideInAt);
+    // const section2 = document.querySelector("#section2");
+    // const section1DistanceToTop = window.pageYOffset + section2.getBoundingClientRect().top;
+    // if (slideInAt > section1DistanceToTop + 60) {
+    //   section2.classList.add("showing");
+    // } else {
+    //   section2.classList.remove("showing");
+    // }
 
-    const section2 = document.querySelector("#section2");
-    const section1DistanceToTop = window.pageYOffset + section2.getBoundingClientRect().top;
-    if (slideInAt > section1DistanceToTop + 60) {
-      section2.classList.add("showing");
+    const developerSection = document.querySelector("#developer");
+    const developerDistanceToTop = window.pageYOffset + developerSection.getBoundingClientRect().top;
+    if (slideInAt > developerDistanceToTop + 60) {
+      developerSection.classList.add("showing");
     } else {
-      section2.classList.remove("showing");
-    }
-
-    const historySection = document.querySelector("#history");
-    const historyDistanceToTop = window.pageYOffset + historySection.getBoundingClientRect().top;
-    if (slideInAt > historyDistanceToTop + 60) {
-      historySection.classList.add("showing");
-    } else {
-      historySection.classList.remove("showing");
+      developerSection.classList.remove("showing");
     }
 
   });
