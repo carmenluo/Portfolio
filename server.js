561272dee60d0387b9a2bb1a114b2a9d01710f74
carmenluo <Sdluojm@gmail.com>
2019-10-22 12:01:10 -0400

    add empty pages to server.js

--- server.js
+++ server.js
@@ -49,6 +49,9 @@ app.use("/api/widgets", widgetsRoutes(db));
 app.get("/", (req, res) => {
   res.render("index");
 });
+app.get("/projects", (req, res) => {
+  res.render("projects");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
