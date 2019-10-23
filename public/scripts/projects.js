6d608cfd1e27f4fae0fec9ae7ed9445e56eeb3c1
carmenluo <Sdluojm@gmail.com>
2019-10-23 17:34:19 -0400

    add scheduler

--- public/scripts/projects.js
+++ public/scripts/projects.js
@@ -1,14 +1,14 @@
-var Expand = (function() {
-  var tile = $('.strips__strip');
-  var tileLink = $('.strips__strip > .strip__content');
-  var tileText = tileLink.find('.strip__inner-text');
-  var stripClose = $('.strip__close');
+let Expand = (function() {
+  let tile = $('.strips__strip');
+  let tileLink = $('.strips__strip > .strip__content');
+  let tileText = tileLink.find('.strip__inner-text');
+  let stripClose = $('.strip__close');
   
-  var expanded  = false;
+  let expanded  = false;
 
-  var open = function() {
+  let open = function() {
       
-    var tile = $(this).parent();
+    let tile = $(this).parent();
 
       if (!expanded) {
         tile.addClass('strips__strip--expanded');
@@ -17,26 +17,20 @@ var Expand = (function() {
         stripClose.addClass('strip__close--show');
         stripClose.css('transition', 'all .6s 1s cubic-bezier(0.23, 1, 0.32, 1)');
         expanded = true;
-      } 
+      } else {
+        tile.removeClass('strips__strip--expanded');
+        // remove delay from inner text
+        tileText.css('transition', 'all 0.2s 0.4s cubic-bezier(0.23, 1, 0.32, 1)');
+        stripClose.removeClass('strip__close--show');
+        stripClose.css('transition', 'all 0.6s 1s cubic-bezier(0.23, 1, 0.32, 1)')
+        expanded = false;
+      }
     };
-  
-  var close = function() {
-    if (expanded) {
-      tile.removeClass('strips__strip--expanded');
-      // remove delay from inner text
-      tileText.css('transition', 'all 0.15s 0 cubic-bezier(0.23, 1, 0.32, 1)');
-      stripClose.removeClass('strip__close--show');
-      stripClose.css('transition', 'all 0.2s 0s cubic-bezier(0.23, 1, 0.32, 1)')
-      expanded = false;
-    }
-  }
-
-    var bindActions = function() {
+    let bindActions = function() {
       tileLink.on('click', open);
-      stripClose.on('click', close);
     };
 
-    var init = function() {
+    let init = function() {
       bindActions();
     };
 
